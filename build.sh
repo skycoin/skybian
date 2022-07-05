f49bf4b1abecaf0c11d793c8aa0fa6c6b9c03bf7
Moses Narrow <moe_narrow@use.startmail.com>
2022-07-05 13:01:00 -0500

    update helper scripts to remove .gz archive creation ; add build.sh ; update checksums

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+# $1 == "0"		only update checksums -> freshly download packages from apt repo
+# $1 == "1"		build the images and compress the image archives .xz and .zst
+# $1 == ""		only build the image ; without compressing
+[[ $1 == "0" ]] && rm *.deb
+./skybian-prime.sh $1
+./skybian-opi3.sh $1
+./skyraspbian-rpi3.sh $1
+./skyraspbian-rpi4.sh $1
