b91bdef707d333fb17b48db5f9edea4d54232957
Moses Narrow <moe_narrow@use.startmail.com>
2022-07-06 08:53:53 -0500

    update versions and checksums

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/bash
 # $1 == "0"		only update checksums -> freshly download packages from apt repo
-# $1 == "1"		build the images and compress the image archives .xz and .zst
-# $1 == ""		only build the image ; without compressing
+# $1 == "1"		only build the image ; without compressing
+# $1 == ""		build the images and compress the image archives .xz and .zst
 [[ $1 == "0" ]] && rm *.deb
 ./skybian-prime.sh $1
 ./skybian-opi3.sh $1
