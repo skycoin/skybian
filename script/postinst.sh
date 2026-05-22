ec8b08282e67853276b69d8ab100e2a61ab2ccbb
Moses Narrow <36607567+0pcom@users.noreply.github.com>
2026-05-22 14:15:40 -0500

    add orange pi prime arch linux arm build script

--- script/postinst.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-#skybian post install script ; executed by dpkg upon package installation or updates
-/usr/bin/skybian-chrootconfig
--- script/skyenv.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-#set envs here
-export SKYBIAN=true
-export VPNSERVER=1
